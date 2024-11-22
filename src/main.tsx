68fbbb9c1ab641559e36a4c82ab792a5c57a3dfe
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2024-11-23 03:41:45 +0600

    material-ui installed and set up

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,8 @@ import { store } from '@/app/store.ts';
 
 import App from './App.tsx';
 
+import '@fontsource/roboto/cyrillic.css';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
