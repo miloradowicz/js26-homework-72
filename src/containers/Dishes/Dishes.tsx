d1df164d5e4a46d0db9b658393ce524b676c5dba
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2024-11-23 10:34:36 +0600

    Dishes added

--- src/containers/Dishes/Dishes.tsx
+++ src/containers/Dishes/Dishes.tsx
@@ -1,5 +1,58 @@
+import { useAppDispatch, useAppSelector } from '@/app/hooks';
+import DishList from '@/components/DishList/DishList';
+import { selectDeleteLoading, selectDishes } from '@/store/slices/dishesSlice';
+import { deleteDish, syncAllDishes } from '@/store/thunks/dishesThunks';
+import { Container } from '@mui/material';
+import { useCallback, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 const Dishes = () => {
-  return <></>;
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const dishes = useAppSelector(selectDishes);
+  const deleteLoading = useAppSelector(selectDeleteLoading);
+
+  useEffect(() => {
+    dispatch(syncAllDishes());
+  }, [dispatch]);
+
+  const editAction = useCallback(
+    (id: string) => {
+      navigate(`/admin/dishes/edit/${id}`);
+    },
+    [navigate]
+  );
+
+  const deleteAction = useCallback(
+    async (id: string) => {
+      await dispatch(deleteDish(id));
+    },
+    [dispatch]
+  );
+
+  const deleteActionState = useCallback(
+    (id: string) => deleteLoading.findIndex((x) => x === id) > 0,
+    [deleteLoading]
+  );
+
+  const extraActions = [
+    {
+      name: 'Edit',
+      action: editAction,
+    },
+    {
+      name: 'Delete',
+      action: deleteAction,
+      state: deleteActionState,
+    },
+  ];
+
+  return (
+    <Container sx={{ p: 2 }}>
+      <DishList dishes={dishes} extraActions={extraActions} />
+    </Container>
+  );
 };
 
 export default Dishes;
